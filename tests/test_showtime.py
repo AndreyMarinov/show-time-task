6b4443192963d393ef14b64218682b26087ca547
Andrey Marinov <heavenlymage97@gmail.com>
2022-10-05 14:17:21 +0300

    Tests for the functions of show_time

--- tests/test_showtime.py
+++ tests/test_showtime.py
@@ -15,6 +15,7 @@ class TestShowTime(unittest.TestCase):
     '''Test for current date'''
     @mock.patch('show_time.datetime')
     def test_current_time(self, datetime_mock):
+        '''Test the curren date in different format'''
         expected = datetime.now().strftime('11:22:40.252670')
         datetime_mock.now().strftime.return_value = '11:22:40.252670'
         print(get_current_time())
@@ -23,23 +24,21 @@ class TestShowTime(unittest.TestCase):
 
     def test_current_date(self):
         '''Test the current date'''
-        expected_day = '2022-09-29'
-        #print(type(expected_day))
-        #print(type(get_current_date()))
+        expected_day = '2022-09-30'
         self.assertEqual(str(get_current_date()), expected_day)
 
 
 
     def test_date_five_days_before(self):
         '''Test substracts 5 days from current day'''
-        with mock.patch('show_time.datetime', return_value = '2022-9-24'):
+        with mock.patch('show_time.datetime', return_value = '2022-9-25'):
             days_to_substract = 5
             assert substract_five_days_from_current_date() == (date.today()
                                                                 - timedelta(days_to_substract))
 
 
     def test_time_now(self):
-        '''Test the current time'''
+        '''Test substract 5 days from current day whitout mocking'''
         days_to_substract = 5
         assert substract_five_days_from_current_date() == (date.today()
                                                              - timedelta(days_to_substract))
