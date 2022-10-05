ebbfea3109434119e4994be84b204ca8cded7b37
Andrey Marinov <heavenlymage97@gmail.com>
2022-10-05 14:15:50 +0300

    Functions that shows the time

--- src/show_time.py
+++ src/show_time.py
@@ -10,15 +10,10 @@ def get_current_time():
     return datetime.now().strftime('%H:%M:%S.%f')
 
 
-print(get_current_time())
-
 
 def get_current_date():
     '''Function that represents the current date in order y/m/d'''
     return date.today()
-
-
-print('Current date:', get_current_date())
 
 
 
@@ -26,7 +21,3 @@ def substract_five_days_from_current_date():
     '''Function that substract five days from the current date'''
     days_to_substract = 5
     return date.today() - timedelta(days_to_substract)
-
-
-print('5 days before the current date:', substract_five_days_from_current_date())
-print(substract_five_days_from_current_date())
